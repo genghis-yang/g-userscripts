b73d708c15ea13fc7933a64c4a97098f20827b43
yangcheng2503 <yangcheng2503@163.com>
2015-04-12 23:00:57 +0800

    Update the userscript to login 163 mailbox. Maybe the last update.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # G-UserScripts
+===================
+
+A user script library for Genghis. They can do some actions automatically on the special web site.
+Create by Genghis Yang. 2015.04.12
+
+**Note**: (2015.09.08)Since the version 35 of Google Chrome, the execution of any extension installed outside of the Google's PlayStore is blocked and cannot be enabled from the extensions menu. There is some workarounds for this but no one is satisfactory. Here are them: [Enable a non-PlayStore UserScript with Chrome 35 and above](http://stackoverflow.com/questions/24095006/enable-a-non-playstore-userscript-with-chrome-35-and-above)
---
+++ mail163/mail163.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name			mail163 
+// @description		Auto-login of 163 mail
+// @version			0.0.1
+// @match			http://mail.163.com/*
+// ==/UserScript==
+// Copyright (c) 2015, Genghis Yang
+
+//replace your username and password in the following expression;
+var MY_USERNAME = "your_user_name@163.com";
+var MY_PASSWORD = "your_password";
+
+function autoFillForm() {
+	var loginForm = document.getElementById("login163");
+	if (loginForm) {
+		var username = document.getElementById("idInput");
+		var password = document.getElementById("pwdInput");
+		var submit = document.getElementById("loginBtn");
+		username.value = MY_USERNAME;
+		password.value = MY_PASSWORD;
+		submit.click();
+	}
+}
+
+window.addEventListener("load", function() {
+	setTimeout(autoFillForm, 0);
+}, false);
